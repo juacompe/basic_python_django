44367fb9972f4ba4ff693b137ee012ef3b9cb2cb
Chokchai Phatharamalai <juacompe@set.(none)>
2013-04-06 15:44:29 +0700

    - refactor factory.create_competitor to take parameters

--- day2/my_first_app/thestar/factory.py
+++ day2/my_first_app/thestar/factory.py
@@ -1,10 +1,7 @@
 from thestar.models import Competitor
 
-def create_competitor():
-    dee = Competitor()
-    dee.name = 'Delilian Alford'
-    dee.nick_name = 'Dee'
-    dee.no = 1 # vote number
-    dee.save()
+def create_competitor(name='Delilian Alford', nick_name='Dee', no=1):
+    dee, is_created = Competitor.objects.get_or_create(
+        name=name, nick_name=nick_name, no=no)
     return dee
 
